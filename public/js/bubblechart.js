5a5dc638de5c181624a5c70cde7164a72311a78e
Randy Lubin <randylubin@gmail.com>
2012-02-28 13:21:24 -0800

    d3 working on /test but not yet on /user

--- public/js/bubblechart.js
+++ public/js/bubblechart.js
@@ -1,1 +1,12 @@
-d3.select("#viz").append("p").text("New paragraph!");
+$(document).ready(function() { 
+	var dataset = [ 5, 10, 15, 20, 25 ];
+	d3.select("#viz").selectAll("div")
+    .data(dataset)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+ 	.style("height", function(d) {
+    	var barHeight = d * 5;  //Scale up by factor of 5
+    	return barHeight + "px";
+	});
+});
