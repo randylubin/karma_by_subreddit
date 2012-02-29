d28a292db15856d646508ce76742b3b2a96b698d
Randy Lubin <randylubin@gmail.com>
2012-02-29 07:36:58 -0800

    a

--- public/js/bubblechart.js
+++ public/js/bubblechart.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	});
 
  	//bubble data
-	var userinfoObject = 
+	/*var userinfoObject = 
 		{
 			"children": [
 			
@@ -25,9 +25,9 @@ $(document).ready(function() {
 				
 			]
 		}
-
+	*/
  	//bubble chart
-
+/*
 	var r = 960,
 	    format = d3.format(",d"),
 	    fill = d3.scale.category20c();
@@ -77,6 +77,6 @@ $(document).ready(function() {
 	  recurse(null, root);
 	  return {children: classes};
 	}
-
+	*/
 });
 
