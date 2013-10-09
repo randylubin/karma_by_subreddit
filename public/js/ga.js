3daa27e3f4351ffedda50e62ed820e74b94593d5
Randy Lubin <randylubin@gmail.com>
2013-10-08 20:27:11 -0700

    updated GA

--- public/js/ga.js
+++ public/js/ga.js
@@ -1,5 +1,6 @@
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-3878446-9']);
+  _gaq.push(['_setAccount', 'UA-21059307-1']);
+  _gaq.push(['_setDomainName', 'randylubin.com']);
   _gaq.push(['_trackPageview']);
 
   (function() {
